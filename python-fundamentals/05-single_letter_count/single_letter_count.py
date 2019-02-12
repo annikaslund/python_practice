d377af1cdbdce96ed84d32eab5f3ebfd54d32b6c
annika <annikaslund@gmail.com>
2019-02-11 16:26:35 -0800

    Complete practice 05

---
+++ python-fundamentals/05-single_letter_count/single_letter_count.py
@@ -0,0 +1,5 @@
+def single_letter_count(str, letter):
+    """ takes in a phrase and a letter, returns 
+    the number of times letter occurs in the phrase """
+
+    return str.lower().count(letter)
